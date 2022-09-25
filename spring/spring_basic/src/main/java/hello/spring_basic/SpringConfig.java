c153d844789ff53347332080d82403e65b6881ca
kimgudals <kimtuti@naver.com>
2022-09-26 00:36:26 +0900

    [22-09-26] DB연결 순수JDBC 사용

--- spring/spring_basic/src/main/java/hello/spring_basic/SpringConfig.java
+++ spring/spring_basic/src/main/java/hello/spring_basic/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.spring_basic;
 
+import hello.spring_basic.repository.JdbcMemberRepository;
 import hello.spring_basic.repository.MemberRepository;
 import hello.spring_basic.repository.MemoryMemberRepository;
 import hello.spring_basic.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 
