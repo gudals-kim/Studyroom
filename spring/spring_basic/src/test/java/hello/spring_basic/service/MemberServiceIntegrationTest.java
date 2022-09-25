20103218117727da90e33e28f205de213fb84e4c
kimgudals <kimtuti@naver.com>
2022-09-26 00:52:10 +0900

    [22-09-26]  스프링 통합 테스트

---
+++ spring/spring_basic/src/test/java/hello/spring_basic/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,60 @@
+package hello.spring_basic.service;
+
+import hello.spring_basic.domain.Member;
+import hello.spring_basic.repository.MemberRepository;
+import hello.spring_basic.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService ;
+    @Autowired MemberRepository memberRepository;
+
+
+    @Test
+    void 회원가입() {
+        //given 이걸 주어졌을때(입력데이터)
+        Member member = new Member();
+        member.setName("hello");
+        //when 이걸 실행했을때
+        Long saveID = memberService.join(member);
+        //then 결과가 이게 나온다
+        Member findMember = memberService.findOne(saveID).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        try{
+//            memberService.join(member2);
+//            fail();
+//        }catch(IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.1234");
+//        }
+
+
+        //then
+    }
+
+}
