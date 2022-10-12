9513c1ca88c009d9722c74a3efa56a67bee1eb55
gudals <kimtuti@naver.com>
2022-10-12 19:16:54 +0900

    [2022-10-12] [springMVC#1] 상품 수정 및 컨트롤러

--- spring/spirng_web/spring_web_itemservice/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring/spirng_web/spring_web_itemservice/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -37,7 +37,6 @@ public class BasicItemController {
         Item findItem = itemRepository.findById(itemId);
         model.addAttribute("item",findItem);
         return "basic/item";
-
     }
     @GetMapping("/add")
     public String addForm(){
@@ -50,8 +49,24 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(
+            @PathVariable Long itemId,
+            Model model
+    ){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
 
-
+    @PostMapping("/{itemId}/edit")
+    public String editItem(
+            @PathVariable Long itemId,
+            @ModelAttribute Item item
+    ){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
 
 
 
