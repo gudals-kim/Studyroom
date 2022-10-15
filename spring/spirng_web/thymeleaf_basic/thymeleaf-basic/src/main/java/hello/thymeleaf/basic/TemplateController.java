a80e23fe6fd358f4715468821841c22973df0189
gudals <kimtuti@naver.com>
2022-10-16 01:51:53 +0900

    [2022-10-15] [springMVC#2] [타임리프 - 기본기능] 레이아웃

--- spring/spirng_web/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ spring/spirng_web/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -14,4 +14,14 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
 }
