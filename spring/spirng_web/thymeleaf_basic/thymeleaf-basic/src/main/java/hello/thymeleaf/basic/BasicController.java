d4fb6ee060f6ae5bc20dfa1718e612b920c429d3
gudals <kimtuti@naver.com>
2022-10-16 01:04:06 +0900

    [2022-10-15] [springMVC#2] [타임리프 - 기본기능] block

--- spring/spirng_web/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ spring/spirng_web/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -119,6 +119,15 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+
+
+
 
     @Component("helloBean")
     static class helloBean{
