821f3186283660af21d580e90117186bda6d4951
gudals <kimtuti@naver.com>
2022-10-16 00:34:51 +0900

    [2022-10-15] [springMVC#2] [타임리프 - 기본기능] 객체~속성 값 컨트롤러

--- spring/spirng_web/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ spring/spirng_web/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -47,6 +50,58 @@ public class BasicController {
 
         return "basic/variable";
     }
+
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Fuck Session");
+        return "basic/basic-object";
+    }
+
+
+    @GetMapping("/date")
+    public String data(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("username","gudals");
+        model.addAttribute("age","22");
+        return  "basic/link";
+
+    }
+
+
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("literal","fuck");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "fuck");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+
+    @Component("helloBean")
+    static class helloBean{
+        public String hello(String data){
+            return "Fuck" + data;
+        }
+    }
+
+
 
 
     @Data
