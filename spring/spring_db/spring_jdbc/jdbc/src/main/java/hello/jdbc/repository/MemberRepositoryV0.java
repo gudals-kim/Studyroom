a46d0a8a46e668bec52267f7996e4d746ee7ddd7
gudals <kimtuti@naver.com>
2022-11-10 00:09:30 +0900

    feat(Jdbc): add Member findbyid feature
    jdbc만을 사용해서 select 쿼리를 날리는 맴버 조회 기능 구현

--- spring/spring_db/spring_jdbc/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ spring/spring_db/spring_jdbc/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 import static hello.jdbc.connection.DBConnectionUtil.*;
 
@@ -31,12 +32,43 @@ public class MemberRepositoryV0 {
             log.error("db error", e);
             throw e;
         } finally {
-
             close(con,pstmt,null);
-
         }
-
     }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";//데이터 조회를 위한 select SQL
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1,memberId);
+
+            //데이터를 변경할 때는 executeUpdate() 를 사용하지만, 데이터를 조회할 때는 executeQuery() 를 사용한다.
+            //executeQuery() 는 결과를 ResultSet 에 담아서 반환한다.
+            //ResultSetd 내부에는 커서가 있다.
+            rs = pstmt.executeQuery();
+            if (rs.next()){//rs.next() : 이것을 호출하면 커서가 다음으로 이동한다.
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));//현재 커서가 가리키고 있는 위치의 member_id 데이터를 String 타입으로 반환한다.
+                member.setMoney(rs.getInt("money"));//현재 커서가 가리키고 있는 위치의 money 데이터를 int 타입으로 반환한다.
+
+                return member;
+            }else {//커서에서 데이터가 없을때?
+                throw new NoSuchElementException("member not found memberId =" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(con,pstmt,rs);
+        }
+    }
+
+
 
     /**
      *쿼리를 실행하고 나면 리소스를 정리해야 한다. 여기서는 Connection , PreparedStatement 를 사용했다.
@@ -50,7 +82,6 @@ public class MemberRepositoryV0 {
      * 만약 이 부분을 놓치게 되면 커넥션이 끊어지지 않고 계속 유지되는 문제가 발생할 수 있다.
      * 이런 것을 리소스 누수라고 하는데, 결과적으로 커넥션 부족으로 장애가 발생할 수 있다.
      */
-
     private void close(Connection con, Statement stmt, ResultSet rs){
         if(rs != null){
             try {
