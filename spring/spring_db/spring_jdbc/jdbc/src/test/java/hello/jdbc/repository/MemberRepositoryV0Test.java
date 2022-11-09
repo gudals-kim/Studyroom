d162d30a66c4d5b5890c64f3471ce7a27b46b7d4
gudals <kimtuti@naver.com>
2022-11-10 00:47:01 +0900

    test(Jdbc): add Member delete feature
    jdbc만을 사용해서 delete 쿼리를 날리는 맴버 삭제 기능 구현

--- spring/spring_db/spring_jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ spring/spring_db/spring_jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,15 +18,27 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV2", 10000);
+        Member member = new Member("memberV3", 10000);
         repository.save(member);
+
         //findbyId
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
+
         //update: money: 10000 -> 20000
         repository.update(member.getMemberId(), 20000);
         Member updatedMember = repository.findById(member.getMemberId());
         assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+/**
+ * 회원을 삭제한 다음 findById() 를 통해서 조회한다.
+ * 회원이 없기 때문에 NoSuchElementException 이 발생한다.
+ * assertThatThrownBy 는 해당 예외가 발생해야 검증에 성공한다
+ */
     }
 }
