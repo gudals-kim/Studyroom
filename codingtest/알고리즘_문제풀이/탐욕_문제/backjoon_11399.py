5f0fa6d8b83b51d2c7d284113f26d839c3dd6274
gudals <kimtuti@naver.com>
2022-11-09 20:27:21 +0900

    feat(greedy): add backjoon_11399 sort feature
    backjoon_11399 정렬기능 추가

--- codingtest/알고리즘_문제풀이/탐욕_문제/backjoon_11399.py
+++ codingtest/알고리즘_문제풀이/탐욕_문제/backjoon_11399.py
@@ -12,3 +12,16 @@ def make_input():
         i += 1
     return result
 
+#정렬기능
+def make_sort(withdrawal_time):
+    if len(withdrawal_time)<=1:
+        return withdrawal_time
+    pivot = withdrawal_time[0]
+    left = list()
+    right = list()
+    for index in range(1,len(withdrawal_time)):
+        if pivot > withdrawal_time[index]:
+            left.append(withdrawal_time[index])
+        else:
+            right.append(withdrawal_time[index])
+    return make_sort(left)+[pivot]+make_sort(right)
