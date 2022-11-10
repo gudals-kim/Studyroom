2036cdfb899ad85067f7133798a8d0b35e159911
gudals <kimtuti@naver.com>
2022-11-10 17:42:05 +0900

    feat(springMVC): add input validation featuresV2
    서버에서 검증하는 입력 검증 방법 V2 th:field, th:errors th:errorclass 사용

--- spring/spirng_web/spring_web_itemservice/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ spring/spirng_web/spring_web_itemservice/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,37 +46,32 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
-
-        //검증 오류 결과를 보관
-        Map<String, String> errors = new HashMap<>();
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())){
             //상품명 공백
-            errors.put("itemName","상품 이름은 필수 입니다.");
+            bindingResult.addError(new FieldError("item","itemName","상품 이름은 필수 입니다."));
         }
         if (item.getPrice() == null || item.getPrice()< 1000 || item.getPrice() >= 1000000){
             //상품 가격이 공백 또는 1,000 ~ 1,000,000 범위를 벗어날 경우
-            errors.put("price","가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","price","가격은 1,000 ~ 1,000,000 까지 허용합니다."));
         }
         if (item.getQuantity() == null || item.getQuantity() > 9999){
             //상품 개수가 공백 또는 9,999 개 이상일 경우
-            errors.put("quantity","수량은 최대 9,999 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","quantity","수량은 최대 9,999 까지 허용합니다."));
         }
         // 틀정 필드가 아닌 복합 룰 검증법
         if (item.getPrice() != null && item.getQuantity() != null){
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000){
-                errors.put("globalError", "등록 상품의 총액이 10,000원 이상이어야 합니다. 현재 상품 등록 총액 = " + resultPrice);
+                bindingResult.addError(new ObjectError("item","등록 상품의 총액이 10,000원 이상이어야 합니다. 현재 상품 등록 총액 = " + resultPrice));
             }
         }
 
         //검증에 실패하면 다시 입력 폼으로
-        if (!errors.isEmpty()){
-            //에러가 없지 않으면 -> 에러가 있으면 (나중에 리펙토링하여 메소드이름으로 수정하면 좋음)
-            log.info("errors = {}",errors);
-            model.addAttribute("errors", errors);
+        if (bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
             return "validation/v2/addForm";
         }
         //이후는 성공(에러가 나지않은) 로직
