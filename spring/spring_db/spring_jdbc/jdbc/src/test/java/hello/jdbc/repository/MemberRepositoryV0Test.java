4c8f86c02eb2b7330a00d3b8a6e801c3c360e53d
gudals <kimtuti@naver.com>
2022-11-22 02:49:13 +0900

    test(jdbc): add connection acquisition with connection pool
    HikariPool 을 사용하여 connection pool 을 만들어 커넥션 획득
    
    커넥션 풀을 사용할때에는 커넥션 풀에 남은 커넥션이 없을시 얼만큼의 시간동안 대기할것인지 설정하는것이 중요하다(짧게하는게 좋음)

--- spring/spring_db/spring_jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ spring/spring_db/spring_jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,37 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
+import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+
 @Slf4j
 class MemberRepositoryV0Test {
 
-    MemberRepositoryV0 repository = new MemberRepositoryV0();
+    MemberRepositoryV0 repository;
+
+    @BeforeEach
+    void beforeEach(){
+        //기본 DriverMannager - 항상 새로운 커넥션을 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL,USERNAME,PASSWORD);
+
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV0(dataSource);
+    }
+
 
     @Test
     void crud() throws SQLException {
