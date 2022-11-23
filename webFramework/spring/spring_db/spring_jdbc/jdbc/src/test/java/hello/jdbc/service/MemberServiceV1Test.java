cd731fc1a48b4e407999228f70a1cef0db9979c1
gudals <kimtuti@naver.com>
2022-11-24 00:14:28 +0900

    test(jdbc): test transaction exam Bank Transfer features
    트랙젝션 이해를 위한 이체 기능 테스트

--- webFramework/spring/spring_db/spring_jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV1Test.java
+++ webFramework/spring/spring_db/spring_jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -3,6 +3,7 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV0;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,7 +33,12 @@ class MemberServiceV1Test {
         memberService = new MemberServiceV1(memberRepository);
     }
 
-
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
 
     @Test
     @DisplayName("정상 이체")
@@ -49,5 +55,25 @@ class MemberServiceV1Test {
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(8000);
         assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+        //when
+        assertThatThrownBy(
+                ()->memberService.accountTransfer(memberA.getMemberId(),memberEx.getMemberId(),2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberEx.getMoney()).isEqualTo(10000);
     }
 }
