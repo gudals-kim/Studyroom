662b7553b744dffec1fee4922415fa0e8ab0676f
gudals <kimtuti@naver.com>
2022-12-06 20:30:59 +0900

    feat(springMVC): add argument resolver @Login annotation
    argument resolver를 활용하여 @Login annotaion 생성

--- webFramework/spring/spirng_web/spring_web_itemservice/login/src/main/java/hello/login/web/HomeController.java
+++ webFramework/spring/spirng_web/spring_web_itemservice/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +25,7 @@ public class HomeController {
     private final SessionManager sessionManager;
     @GetMapping("/")
     public String homeLogin(
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+            @Login Member loginMember, Model model) {
         //로그인
         if (loginMember == null){
             return "home";
