2bc924113ca3f231812cfdf66d4b19e8a26034c2
gudals <kimtuti@naver.com>
2022-12-15 21:05:57 +0900

    refactor(CT):edit backjoon 10816

--- codingtest/알고리즘_문제풀이/정렬_문제/backjoon_10816.py
+++ codingtest/알고리즘_문제풀이/정렬_문제/backjoon_10816.py
@@ -1,6 +1,16 @@
+import time
+import datetime
+import psutil
 import sys
-sys.stdin = open("/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/정렬_문제/input/input10816.txt","r")
-
+def memory_usage():
+    p = psutil.Process()
+    rss = p.memory_info().rss / 2 ** 20 # Bytes to MB
+    return rss
+inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/정렬_문제/input/input10816.txt"
+sys.stdin = open(inputData,"r")
+startTime = time.time()
+print("--------답안 코드입니다.--------")
+# ======== 답안지 작성을 합니다 =========
 n = sys.stdin.readline()
 nCard = list(map(int,sys.stdin.readline().split()))
 m = sys.stdin.readline()
@@ -14,3 +24,9 @@ for num in nCard:
     else:
         result[num] = 1
 print(' '.join(str(result[x]) if x in result else '0' for x in mCard))
+# ==================================
+print("--------답안 코드입니다.--------")
+endTime = time.time()
+
+print(f"code memory usage  {memory_usage(): 10.5f} MB")
+print(f"code execution time  {datetime.timedelta(seconds=(endTime - startTime))} sec")
