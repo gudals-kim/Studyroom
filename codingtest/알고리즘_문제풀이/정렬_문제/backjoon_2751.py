f235c8306c4451585d5cfe5d23ca02c856596583
gudals <kimtuti@naver.com>
2023-01-04 16:48:06 +0900

    feat(CT):solve backjoon 2751

---
+++ codingtest/알고리즘_문제풀이/정렬_문제/backjoon_2751.py
@@ -0,0 +1,28 @@
+import time
+import datetime
+import psutil
+import sys
+inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/정렬_문제/input/backjoon_2751.txt"
+
+p = psutil.Process()
+
+sys.stdin = open(inputData,"r")
+T = int(sys.stdin.readline())
+for test_case in range(1,T+1):
+    startTime = time.time()
+    print("--------여기부터 답안 출력입니다.--------")
+    # ======== 답안지 작성을 합니다 =========
+    import heapq
+    반복 = int(sys.stdin.readline())
+    data = []
+    for _ in range(반복):
+        heapq.heappush(data,int(sys.stdin.readline()))
+    for _ in range(반복):
+        print(heapq.heappop(data))
+    # ==================================
+    print("--------여기까지 답안 출력입니다.--------")
+    rss = p.memory_info().rss / 2 ** 20  # Bytes to MB
+    endTime = time.time()
+
+    print(f" {test_case} testCode memory usage  {rss: 10.5f} MB")
+    print(f" {test_case} testCode execution time  {datetime.timedelta(seconds=(endTime - startTime))} sec")
