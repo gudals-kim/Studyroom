42dc3d9922b134a48136f7af227ea5d790514828
gudals <kimtuti@naver.com>
2023-01-10 20:42:15 +0900

    feat(CT):solve backjoon 1939

--- codingtest/알고리즘_문제풀이/탐색_문제/exc/backjoon_1939.py
+++ codingtest/알고리즘_문제풀이/탐색_문제/exc/backjoon_1939.py
@@ -12,13 +12,41 @@ for test_case in range(1,T+1):
     startTime = time.time()
     print("--------여기부터 답안 출력입니다.--------")
     # ======== 답안지 작성을 합니다 =========
+    from collections import deque
+    n, m = map(int, input().split())
+    adj = [[] for _ in range(n+1)]
+    def bfs(c):
+        queue = deque([start_node])
+        visited = [False] * (n+1)
+        visited[start_node] = True
+        while queue:
+           x = queue.popleft()
+           for y, weight in adj[x]:
+               if not visited[y] and weight >= c:
+                    visited[y] = True
+                    queue.append(y)
+        return visited[end_node]
 
+    start = 1000000000
+    end = 1
 
+    for _ in range(m):
+        x, y ,weight = map(int, input().split())
+        adj[x].append((y, weight))
+        adj[y].append((x, weight))
+        start = min(start, weight)
+        end = max(end, weight)
 
-
-
-
-
+    start_node, end_node = map(int, input().split())
+    result = start
+    while(start<=end):
+        mid = (start + end)//2
+        if bfs(mid):
+            result = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+    print(result)
 
     # ==================================
     print("--------여기까지 답안 출력입니다.--------")
