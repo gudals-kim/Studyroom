858dbfbca6c743ef1b4b16c48c643223b76e34ad
gudals <kimtuti@naver.com>
2023-01-11 20:28:41 +0900

    feat(CT): solve backjoon 1991

---
+++ codingtest/알고리즘_문제풀이/탐색_문제/exc/backjoon_1991.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+import psutil
+import sys
+inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/탐색_문제/input/backjoon_1991.txt"
+
+p = psutil.Process()
+
+sys.stdin = open(inputData,"r")
+T = int(sys.stdin.readline())
+for test_case in range(1,T+1):
+    startTime = time.time()
+    print("--------여기부터 답안 출력입니다.--------")
+    # ======== 답안지 작성을 합니다 =========
+
+    # ==================================
+    print("--------여기까지 답안 출력입니다.--------")
+    rss = p.memory_info().rss / 2 ** 20  # Bytes to MB
+    endTime = time.time()
+
+    print(f" {test_case} testCode memory usage  {rss: 10.5f} MB")
+    print(f" {test_case} testCode execution time  {datetime.timedelta(seconds=(endTime - startTime))} sec")
