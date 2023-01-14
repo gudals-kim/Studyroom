8e7769769e96f0a93a9527cb72db0521483ad4ca
gudals <kimtuti@naver.com>
2023-01-15 02:53:34 +0900

    feat(jdbc): resolving JDBC iteration issues
    스프링과 문제 해결 - 예외 처리, 반복 (jdbc 템플릿 사용)
    
    정리
    트랜잭션 추상화 + 트랜잭션 AOP 덕분에 서비스 계층의 순수성을 최대한 유지하면서 서비스 계층에서 트랜잭션을 사용할 수 있다.
    스프링이 제공하는 예외 추상화와 예외 변환기 덕분에, 데이터 접근 기술이 변경되어도 서비스 계층의 순수성을 유지하면서 예외도 사용할 수 있다.
    서비스 계층이 리포지토리 인터페이스에 의존한 덕분에 향후 리포지토리가 다른 구현 기술로 변경되어도 서비스 계층을 순수하게 유지할 수 있다.
    리포지토리에서 JDBC를 사용하는 반복 코드가 JdbcTemplate 으로 대부분 제거되었다.

--- webFramework/spring/spring_db/spring_jdbc/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ webFramework/spring/spring_db/spring_jdbc/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -1,152 +1,49 @@
 package hello.jdbc.repository;
 
-
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.ex.MyDbException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
-import org.springframework.jdbc.support.SQLExceptionTranslator;
-
-
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import javax.sql.DataSource;
-import java.sql.*;
-import java.util.NoSuchElementException;
-
-
 
 /**
- * SQLException Translator 추가
+ * JDBCTemplate 사용
  */
 @Slf4j
 public class MemberRepositoryV0 implements MemberRepository{
 
-    private final DataSource dataSource;
-    private final SQLExceptionTranslator exTranslator;
+    private final JdbcTemplate template;
 
     public MemberRepositoryV0(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+        this.template = new JdbcTemplate(dataSource);
     }
     @Override
     public void delete(String memberId){
         String sql = "delete from member where member_id=?";
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, memberId);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            throw exTranslator.translate("delete",sql,e);
-        } finally {
-            close(con, pstmt, null);
-        }
+        template.update(sql, memberId);
     }
     @Override
     public void update(String memberId, int money) {
         String sql = "update member set money=? where member_id=?";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = getConnection();//DBConnectionUtil 를 통해서 데이터베이스 커넥션을 획득한다.
-            pstmt = con.prepareStatement(sql);//데이터베이스에 전달할 SQL과 파라미터로 전달할 데이터들을 준비
-            pstmt.setInt(1, money);
-            pstmt.setString(2, memberId);
-
-            //executeUpdate() 는 쿼리를 실행하고 영향받은 row수를 반환한다.
-            // 여기서는 하나의 데이터만 변경하기 때문에 결과로 1이 반환된다.
-            // 만약 회원이 100명이고, 모든 회원의 데이터를 한번에 수정하는 update sql 을 실행하면 결과는 100이 된다.
-            int resultSize = pstmt.executeUpdate();
-            log.info("resultSize={}",resultSize);
-        } catch (SQLException e) {
-            throw exTranslator.translate("update",sql,e);
-        } finally {
-            close(con,pstmt,null);
-        }
+        template.update(sql, money, memberId);
     }
     @Override
     public Member save(Member member) {
-        String sql = "insert into member(member_id, money) values (?,?)";//데이터베이스에 전달할 SQL을 정의
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();//DBConnectionUtil 를 통해서 데이터베이스 커넥션을 획득한다.
-            pstmt = con.prepareStatement(sql);//데이터베이스에 전달할 SQL과 파라미터로 전달할 데이터들을 준비
-            pstmt.setString(1, member.getMemberId());//SQL의 첫번째 ? 에 값을 지정한다. 문자이므로 setString 을 사용한다.
-            pstmt.setInt(2, member.getMoney());//SQL의 두번째 ? 에 값을 지정한다. Int 형 숫자이므로setInt 를 지정한다.
-            pstmt.executeUpdate();//Statement 를 통해 준비된 SQL을 커넥션을 통해 실제 데이터베이스에 전달한다
-            return member;
-        } catch (SQLException e) {
-            throw exTranslator.translate("save",sql,e);
-        } finally {
-            close(con,pstmt,null);
-        }
+        String sql = "insert into member(member_id, money) values (?,?)";
+        template.update(sql, member.getMemberId(), member.getMoney());
+        return member;
     }
     @Override
     public Member findById(String memberId) {
-        String sql = "select * from member where member_id = ?";//데이터 조회를 위한 select SQL
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1,memberId);
-
-            //데이터를 변경할 때는 executeUpdate() 를 사용하지만, 데이터를 조회할 때는 executeQuery() 를 사용한다.
-            //executeQuery() 는 결과를 ResultSet 에 담아서 반환한다.
-            //ResultSetd 내부에는 커서가 있다.
-            rs = pstmt.executeQuery();
-            if (rs.next()){//rs.next() : 이것을 호출하면 커서가 다음으로 이동한다.
-                Member member = new Member();
-                member.setMemberId(rs.getString("member_id"));//현재 커서가 가리키고 있는 위치의 member_id 데이터를 String 타입으로 반환한다.
-                member.setMoney(rs.getInt("money"));//현재 커서가 가리키고 있는 위치의 money 데이터를 int 타입으로 반환한다.
-
-                return member;
-            }else {//커서에서 데이터가 없을때?
-                throw new NoSuchElementException("member not found memberId =" + memberId);
-            }
-        } catch (SQLException e) {
-            throw exTranslator.translate("findById",sql,e);
-        }finally {
-            close(con,pstmt,rs);
-        }
+        String sql = "select * from member where member_id = ?";
+        return template.queryForObject(sql, memberRowMapper(), memberId);
     }
-
-
-    /**
-     *쿼리를 실행하고 나면 리소스를 정리해야 한다. 여기서는 Connection , PreparedStatement 를 사용했다.
-     *리소스를 정리할 때는 항상 역순으로 해야한다.
-     *Connection 을 먼저 획득하고 Connection 을 통해 PreparedStatement 를 만들었기 때문에
-     *리소스를 반환할 때는 PreparedStatement 를 먼저 종료하고, 그 다음에 Connection 을 종료하면 된다.
-     *참고로 여기서 사용하지 않은 ResultSet 은 결과를 조회할 때 사용한다.
-     *
-     * <주의>
-     * 리소스 정리는 꼭! 해주어야 한다. 따라서 예외가 발생하든, 하지 않든 항상 수행되어야 하므로 finally 구문에 주의해서 작성해야한다.
-     * 만약 이 부분을 놓치게 되면 커넥션이 끊어지지 않고 계속 유지되는 문제가 발생할 수 있다.
-     * 이런 것을 리소스 누수라고 하는데, 결과적으로 커넥션 부족으로 장애가 발생할 수 있다.
-     */
-    private void close(Connection con, Statement stmt, ResultSet rs){
-        JdbcUtils.closeResultSet(rs);
-        JdbcUtils.closeStatement(stmt);
-
-        //트랜잭션 동기화를 사용할려면 DataSourceUtils에서 사용해야한다.
-        DataSourceUtils.releaseConnection(con,dataSource);
-
-    }
-
-
-    private Connection getConnection() throws SQLException {
-        //트랜잭션 동기화를 사용하려면 DataSourceUtils에서 사용해야한다.
-        Connection con = DataSourceUtils.getConnection(dataSource);
-        log.info("get connection={}, class={}",con,con.getClass());
-        return con;
+    private RowMapper<Member> memberRowMapper(){
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setMemberId(rs.getString("member_id"));
+            member.setMoney(rs.getInt("money"));
+            return member;
+        };
     }
 }
