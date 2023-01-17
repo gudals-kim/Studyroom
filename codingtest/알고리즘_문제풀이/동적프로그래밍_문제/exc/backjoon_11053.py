9004b5188408fccf8a5c14e8a1b2255e0c2b6f12
gudals <kimtuti@naver.com>
2023-01-18 01:15:54 +0900

    feat(CT): add backjoon 11053 solve files

--- codingtest/알고리즘_문제풀이/동적프로그래밍_문제/exc/backjoon_11053.py
+++ codingtest/알고리즘_문제풀이/동적프로그래밍_문제/exc/backjoon_11053.py
@@ -2,7 +2,7 @@ import time
 import datetime
 import psutil
 import sys
-inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/탐색_문제/input/backjoon_11053.txt"
+inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/동적프로그래밍_문제/input/backjoon_11053.txt"
 
 p = psutil.Process()
 
@@ -12,7 +12,14 @@ for test_case in range(1,T+1):
     startTime = time.time()
     print(f"--------{test_case}번 테스트 코드 답안 출력입니다.--------")
     # ======== 답안지 작성을 합니다 =========
-
+    n = int(input())
+    array = list(map(int, input().split()))
+    dp = [1]*n
+    for i in range(1, n):
+        for j in range(0, i):
+            if array[j] < array[i]:
+                dp[i] = max(dp[i], dp[j]+1)
+    print(max(dp))
     # ==================================
     print(f"-----------------------------------------")
     rss = p.memory_info().rss / 2 ** 20  # Bytes to MB
