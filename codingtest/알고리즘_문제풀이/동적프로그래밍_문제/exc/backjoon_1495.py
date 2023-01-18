87d82f1beb1d7b3d37457305a6eaacc9752d35c4
gudals <kimtuti@naver.com>
2023-01-18 20:11:27 +0900

    feat(CT): add backjoon 1495 solve files

--- codingtest/알고리즘_문제풀이/동적프로그래밍_문제/exc/backjoon_1495.py
+++ codingtest/알고리즘_문제풀이/동적프로그래밍_문제/exc/backjoon_1495.py
@@ -2,7 +2,7 @@ import time
 import datetime
 import psutil
 import sys
-inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/탐색_문제/input/backjoon_1495.txt"
+inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/동적프로그래밍_문제/input/backjoon_1495.txt"
 
 p = psutil.Process()
 
@@ -12,7 +12,26 @@ for test_case in range(1,T+1):
     startTime = time.time()
     print(f"--------{test_case}번 테스트 코드 답안 출력입니다.--------")
     # ======== 답안지 작성을 합니다 =========
+    n, s, m = map(int, input().split())
+    array = list(map(int, input().split()))
 
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[0][s] = 1
+
+    for i in range(1, n + 1):
+        for j in range(m + 1):
+            if dp[i - 1][j] == 0:
+                continue
+            if j - array[i - 1] >= 0:
+                dp[i][j - array[i - 1]] = 1
+            if j + array[i - 1] <= m:
+                dp[i][j + array[i - 1]] = 1
+    result = -1
+    for i in range(m, -1, -1):
+        if dp[n][i] == 1:
+            result = i
+            break
+    print(result)
     # ==================================
     print(f"-----------------------------------------")
     rss = p.memory_info().rss / 2 ** 20  # Bytes to MB
