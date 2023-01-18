35d47f626f2e462845218eb40217b70b0f163804
gudals <kimtuti@naver.com>
2023-01-19 01:21:24 +0900

    feat(springMVC): add spring server converter
    컨버터를 추가하면 추가한 컨버터가 기본 컨버터 보다 높은 우선순위를 가진다
    처리 과정
    @RequestParam 은 @RequestParam 을 처리하는 ArgumentResolver 인
    RequestParamMethodArgumentResolver 에서 ConversionService 를 사용해서 타입을 변환한다.

--- webFramework/spring/spirng_web/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ webFramework/spring/spirng_web/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,12 @@ public class HelloController {
         return "ok";
     }
 
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("ipPort.getIp() = " + ipPort.getIp());
+        System.out.println("ipPort.getPort() = " + ipPort.getPort());
+        return "ok";
+    }
 
 
 }
