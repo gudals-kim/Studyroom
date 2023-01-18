1df76094fb399923561424ad2b6394e5382b6d96
gudals <kimtuti@naver.com>
2023-01-18 18:15:58 +0900

    feat(CT): add backjoon 9251 solve files

--- codingtest/알고리즘_문제풀이/동적프로그래밍_문제/exc/backjoon_9251.py
+++ codingtest/알고리즘_문제풀이/동적프로그래밍_문제/exc/backjoon_9251.py
@@ -2,7 +2,7 @@ import time
 import datetime
 import psutil
 import sys
-inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/탐색_문제/input/backjoon_9251.txt"
+inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/동적프로그래밍_문제/input/backjoon_9251.txt"
 
 p = psutil.Process()
 
@@ -12,6 +12,17 @@ for test_case in range(1,T+1):
     startTime = time.time()
     print(f"--------{test_case}번 테스트 코드 답안 출력입니다.--------")
     # ======== 답안지 작성을 합니다 =========
+    x = input()
+    y = input()
+    dp = [[0]*(len(y)+1) for _ in range(len(x)+1)]
+
+    for i in range(1, len(x)+1):
+        for j in range(1, len(y)+1):
+            if x[i-1]==y[j-1]:
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+    print(dp[len(x)][len(y)])
 
     # ==================================
     print(f"-----------------------------------------")
