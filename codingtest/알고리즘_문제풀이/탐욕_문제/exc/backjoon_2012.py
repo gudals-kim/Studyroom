e32a5a334a41b45e15e13dd007c365550adb9cf8
gudals <kimtuti@naver.com>
2023-02-03 04:09:17 +0900

    feat(CT): setup backjoon 2012 solve files

---
+++ codingtest/알고리즘_문제풀이/탐욕_문제/exc/backjoon_2012.py
@@ -0,0 +1,23 @@
+import time
+import datetime
+import psutil
+import sys
+inputData = "/Users/gudals/Documents/github/Studyroom/codingtest/알고리즘_문제풀이/탐욕_문제/input/backjoon_2012.txt"
+
+p = psutil.Process()
+
+sys.stdin = open(inputData,"r")
+T = int(sys.stdin.readline())
+for test_case in range(1,T+1):
+    startTime = time.time()
+    print(f"--------{test_case}번 테스트 코드 답안 출력입니다.--------")
+    # ======== 답안지 작성을 합니다 =========
+
+    # ==================================
+    print(f"-----------------------------------------")
+    rss = p.memory_info().rss / 2 ** 20  # Bytes to MB
+    endTime = time.time()
+
+    print(f" {test_case} testCode memory usage  {rss: 10.5f} MB")
+    print(f" {test_case} testCode execution time  {datetime.timedelta(seconds=(endTime - startTime))} sec")
+    print(f"-----------------------------------------")
