439ed193a3e7091e3dd9bbd3cef0dd97815698d7
gudals <kimtuti@naver.com>
2023-02-03 22:27:59 +0900

    feat(CT): solve backjoon 5585 solve files

--- codingtest/알고리즘_문제풀이/탐욕_문제/exc/backjoon_5585.py
+++ codingtest/알고리즘_문제풀이/탐욕_문제/exc/backjoon_5585.py
@@ -12,7 +12,12 @@ for test_case in range(1,T+1):
     startTime = time.time()
     print(f"--------{test_case}번 테스트 코드 답안 출력입니다.--------")
     # ======== 답안지 작성을 합니다 =========
-
+    m = 1000-int(input())
+    result = 0
+    for n in [500, 100, 50, 10 , 5, 1]:
+        result += m//n
+        m = m%n
+    print(result)
     # ==================================
     print(f"-----------------------------------------")
     rss = p.memory_info().rss / 2 ** 20  # Bytes to MB
