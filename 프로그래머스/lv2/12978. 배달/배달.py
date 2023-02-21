35fab340f5ef3999f5bd60640c8ed51f4e4cc430
hyeong min <rlagudals0420@naver.com>
2023-02-21 19:09:52 +0900

    [level 2] Title: 배달, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.py
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -1,28 +1,29 @@
-from collections import deque, defaultdict
+from collections import defaultdict
 import heapq
 
 def solution(N, road, K):
     answer = 0
     graph = defaultdict(list)
     for node in road:
-        graph[node[0]].append((node[2],node[1]))
-        graph[node[1]].append((node[2],node[0]))
-    최단거리 = { n : float('inf') for n in graph.keys()}
-    최단거리[1]=0
+        graph[node[0]].append([node[2],node[1]])
+        graph[node[1]].append([node[2],node[0]])
+    최단거리들 = { node : float('inf') for node in graph.keys() }
     heap = []
-    heapq.heappush(heap,(최단거리[1],1))
-    
+    최단거리들[1]=0
+    heapq.heappush(heap,[최단거리들[1],1])
     while heap:
-        현재노드까지의최단거리, 현재노드 =heapq.heappop(heap)
-        if 최단거리[현재노드] < 현재노드까지의최단거리:
+        현재노드까지의최단거리, 현재노드 = heapq.heappop(heap)
+        if 현재노드까지의최단거리 > 최단거리들[현재노드]:
             continue
-        for 현재노드와다음노드의거리, 다음노드 in graph[현재노드]:
-            다음노드까지의최단거리 = 현재노드와다음노드의거리 + 현재노드까지의최단거리
-            if 다음노드까지의최단거리 < 최단거리[다음노드]:
-                최단거리[다음노드] = 다음노드까지의최단거리
-                heapq.heappush(heap,[다음노드까지의최단거리,다음노드])
-    for n in 최단거리.values():
-        if n <= K:
-            answer +=1
+        for 다음노드까지의거리, 다음노드 in graph[현재노드]:
+            다음노드까지의최단거리 = 다음노드까지의거리 + 현재노드까지의최단거리
+            if 다음노드까지의최단거리 < 최단거리들[다음노드]:
+                최단거리들[다음노드] = 다음노드까지의최단거리
+                heapq.heappush(heap,[최단거리들[다음노드],다음노드])
+    
+    for dist in 최단거리들.values():
+        if dist <= K:
+            answer+=1
+    
     
     return answer
