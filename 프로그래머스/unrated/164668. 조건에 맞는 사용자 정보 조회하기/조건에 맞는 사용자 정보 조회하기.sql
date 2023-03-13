ef7d946c8f22b2334272b49a5094c346ac7a52a0
hyeong min <rlagudals0420@naver.com>
2023-03-13 22:38:15 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164668. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/unrated/164668. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -3,5 +3,5 @@ SELECT b.USER_ID, b.NICKNAME, sum(a.price) as TOTAL_SALES
 from USED_GOODS_BOARD a join USED_GOODS_USER b on a.WRITER_ID = b.USER_ID
 where a.status='DONE'
 group by a.writer_id
-having sum(a.price)>=700000
+having TOTAL_SALES>=700000
 order by TOTAL_SALES
