8793cb41a0a22604a54b98fc0384a2d1242a8ec7
hyeong min <rlagudals0420@naver.com>
2023-05-04 17:40:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -3,12 +3,12 @@ from collections import defaultdict
 def solution(id_list, report, k):
     id_list_to_idx = { id_list[idx] : idx for idx in range(len(id_list))}
     answer = [0] * len(id_list)
-    신고리스트 = {ids : {idx:0 for idx in id_list} for ids in id_list}
     이사람을신고한사람들 = defaultdict(set)
+    
     for repo in report:
         신고자, 피신고자 = repo.split()
         이사람을신고한사람들[피신고자].add(신고자)
-        신고리스트[피신고자][신고자]+=1
+
     for 피신고자 in id_list:
         if len(이사람을신고한사람들[피신고자]) >= k:
             for 신고자 in 이사람을신고한사람들[피신고자]:
