89aa964ecc49889098054cdc610b3c8b9dea3d55
hyeong min <rlagudals0420@naver.com>
2023-08-08 16:31:05 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 121 ms, Memory: 20,480 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,38 @@
+
+
+import java.io.*;
+import java.util.*;
+public class Solution {
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws  IOException{
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+        for (int t = 1; t <= 10; t++) {
+            sb.append("#").append(t).append(" ");
+            int n = Integer.parseInt(br.readLine());
+            int ans = 1;
+
+            for (int i = 0; i < n; i++) {
+                String line = br.readLine();
+                st = new StringTokenizer(line);
+                st.nextToken();
+                String code = st.nextToken();
+                if (!st.hasMoreTokens()) {
+                    if (code.equals("-")||code.equals("+")||code.equals("*")||code.equals("/")){
+                        ans=0;
+                    }
+                    continue;
+                }
+                if (code.equals("-")||code.equals("+")||code.equals("*")||code.equals("/"))continue;
+                ans = 0;
+            }
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
