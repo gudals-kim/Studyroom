5dde0751299979a42c41c650703984ceefb1bc82
hyeong min <rlagudals0420@naver.com>
2023-08-11 11:26:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int ans=0;
+        while (N>=0){
+            if (N%5==0){
+                ans+=N/5;
+                sb.append(ans);
+                break;
+            }
+            N -= 3;
+            ans ++;
+        }
+        if (N<0){
+            sb.append(-1);
+        }
+        System.out.println(sb);
+
+
+
+
+    }
+}
