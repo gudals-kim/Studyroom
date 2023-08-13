0d68113f1457fe16256ddce998e8d5bc92285502
hyeong min <rlagudals0420@naver.com>
2023-08-14 02:11:38 +0900

    [Silver III] Title: N과 M (7), Time: 632 ms, Memory: 220600 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        perm(arr,M,0,new int[M]);
+        System.out.println(sb);
+
+    }
+    static void perm(int[] arr, int r, int cnt, int[] permCase){
+
+        if (cnt==r){
+            for (int i : permCase) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            permCase[cnt] = arr[i];
+            perm(arr, r, cnt+1, permCase);
+        }
+
+    }
+
+
+}
