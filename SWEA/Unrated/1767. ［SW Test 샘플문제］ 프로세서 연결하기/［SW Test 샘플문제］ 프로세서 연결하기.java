5c6afc1edd7e86eca7f572145b3406e46e3ce1a3
hyeong min <rlagudals0420@naver.com>
2023-10-11 17:08:09 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 353 ms, Memory: 87,436 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,12 +1,12 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
+
 public class Solution {
-    static int N;
-    static int[][] move = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
-    static int[][] map;
-    static int maxCore, minLength;
+    static int N, ans, ansCount;
+    static int[][] move = {{-1,0},{0,1},{1,0},{0,-1}};
     static ArrayList<Core> cores;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
@@ -15,97 +15,72 @@ public class Solution {
         for (int t = 1; t <= T; t++) {
             sb.append("#").append(t).append(" ");
             N = Integer.parseInt(br.readLine());
-
-
-            //입력 받기 start
-            map = new int[N][N];
+            int[][] map = new int[N][N];
+            boolean[][] visit = new boolean[N][N];
             cores = new ArrayList<>();
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
-                    if(i == 0 || j == 0 || i == N-1 || j == N-1)
-                        // 가장가리에 있을때는 무조건 길이가 0이여야해
-                        continue;
-                    if(map[i][j] == 1)
+                    if (map[i][j]==1) {
+                        visit[i][j] = true;
+                        if (i==0||i==N-1||j==0||j==N-1) continue;
                         cores.add(new Core(i,j));
+                    }
                 }
             }
-            //입력 받기 end
-
-            maxCore = Integer.MIN_VALUE;
-            minLength = Integer.MAX_VALUE;
-            dfs(0,0,0);
-            sb.append(minLength).append("\n");
+            ans = 999999;
+            ansCount = -1;
+            dfs(0,0,visit,0);
+            sb.append(ans).append("\n");
         }
         System.out.println(sb);
     }
+    static void dfs(int idx, int len, boolean[][] visit, int conCount){
 
-    static void dfs(int coreNum, int coreCnt, int len){
-        if (coreNum == cores.size()){//모든 코어를 다 선택했을때
-            if (maxCore < coreCnt){ //선택된 코어 개수가 더 많을때
-                maxCore = coreCnt;// 최대값 갱신
-                minLength = len; //최소 길이 기준 갱신
-            } else if (maxCore == coreCnt) {
-                //가장 많은 코어 개수를 선택했을때 가장 전선이 짧은 때로 갱신한다.
-                minLength = Math.min(minLength, len);
+        if (idx==cores.size()){
+            if (ansCount<conCount){
+                ansCount = conCount;
+                ans = len;
+            }
+            if (ansCount==conCount){
+                ans = Math.min(ans, len);
             }
             return;
         }
-        //남은 코어가 있을때,
 
-        int x = cores.get(coreNum).x;
-        int y = cores.get(coreNum).y;
+        Core core = cores.get(idx);
 
-        //모든 4방향 탐색
-        for (int pointer = 0; pointer < 4; pointer++) {
-            int dx = x;
-            int dy = y;
-            int moveCnt = 0; // 이동횟수 저장
-
-            while (true){
-                dx += move[pointer][0];
-                dy += move[pointer][1];
-                // 벽을 만날 때 까지 ! -> 만난다면 전선 연결할 수 있음
-                if (isWall(dx, dy)) break;
-                // 전선을 만난다면
-                if (map[dx][dy]==1){
-                    moveCnt=0; // 전선을 벽까지 연결 할 수 없음
+        //프로세스 연결하기
+        for (int[] mv : move) {
+            int dx = mv[0] + core.x;
+            int dy = mv[1] + core.y;
+            boolean con = false;
+            ArrayList<Core> visitNode = new ArrayList<>();
+            while (dx>=0&&dx<N&&dy>=0&&dy<N){
+                if (visit[dx][dy]) { // 연결 못하는 경우
+                    con = false;
                     break;
                 }
-                moveCnt++; // 전선길이 증가
+                //연결 가능한 경우
+                visitNode.add(new Core(dx,dy));
+                con = true;
+                dx += mv[0];
+                dy += mv[1];
             }
-
-            //전선이 연결된 경우
-            if (moveCnt!=0) {
-                int mx = x;
-                int my = y;
-                for (int i = 0; i < moveCnt; i++) {
-                    mx += move[pointer][0];
-                    my += move[pointer][1];
-                    map[mx][my] = 1; //전선이 연결되었으면 map에 갱신한다
+            if (con){ // 연결가능한 경우
+                for (Core node : visitNode) {// 연결 처리
+                    visit[node.x][node.y] = true;
                 }
-                //다음 코어로 이동한다.
-                dfs(coreNum+1, coreCnt+1, len + moveCnt);
-                mx = x;
-                my = y;
-
-                //dfs가 끝난뒤에는 연결된 map를 0으로 바꿔줘야함
-                // -> 분기점이 다름. 다른 전선을 연결할거임
-                for (int i = 0; i < moveCnt; i++) {
-                    mx += move[pointer][0];
-                    my += move[pointer][1];
-                    map[mx][my] = 0;
+                dfs(idx+1,len+visitNode.size(), visit, conCount+1); // 다음 코어 연결하러 가기
+                for (Core node : visitNode) {// 다시 끊어주기
+                    visit[node.x][node.y] = false;
                 }
-            }else {
-                //전선과 연결할 수 없을때는 코어만 바꾸어주고 코어개수와 길이를 증가 및 갱신하지 않는다.
-                dfs(coreNum+1, coreCnt, len);
             }
         }
+        dfs(idx+1, len, visit, conCount);
     }
-    static boolean isWall(int x, int y){
-        return (x < 0 || x >= N || y < 0 || y >= N);
-    }
+
 }
 class Core{
     int x;
