436b61fa3e0ce0a5622a259b7da0ae53c9d000e5
hyeong min <rlagudals0420@naver.com>
2024-02-23 09:18:31 +0900

    [Silver IV] Title: 제로, Time: 196 ms, Memory: 21064 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,27 +1,18 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int K = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
-        int ans = 0;
+        int K = Integer.parseInt(br.readLine());
+        int sum = 0;
         Stack<Integer> stack = new Stack<>();
 
-        for (int k=0; k<K; k++){
-            int n = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
-            if (n==0){
-                stack.pop();
-                continue;
-            }
-            stack.push(n);
+        for (int i = 0; i < K; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sum = (n == 0) ? sum - stack.pop() : sum + stack.push(n);
         }
-
-        for (Integer integer : stack) {
-            ans+=integer;
-        }
-        System.out.print(ans);
+        System.out.println(sum);
     }
 }
